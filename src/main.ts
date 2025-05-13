57e61473d06a20937466ab6b0620034dcd0de01c
Lauramgr22 <lauramgr6@gmail.com>
2025-05-13 15:47:33 -0500

    feat: version 1

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,29 @@
-//Codigo principal
+import { app, BrowserWindow } from 'electron';
+import * as path from 'path';
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    fullscreen: true,
+    frame: false,
+    alwaysOnTop: true,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  win.loadFile(path.join(__dirname, '../public/index.html'));
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
